4df16edd6312ddea8984be9469a8687398bce8a9
Vaidehi Patel <vaidehipatel31101@gmail.com>
2024-05-04 12:36:42 +0530

    sequelizerc implemented

---
+++ backend/db/seeders/20240504070514-demo-user.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [
+      {
+        firstName: 'Vaidehi',
+        lastName: 'Patel',
+        email: 'vaidehi@abc.com',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', null, {});
+  },
+};
